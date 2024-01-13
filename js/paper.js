582e862a5e2abd571538a007c29285d87111501f
anig1scur <anigiscur@gmail.com>
2024-01-14 01:19:33 +0800

    Site updated: 2024-01-14 01:19:32

--- js/paper.js
+++ js/paper.js
@@ -1,19 +1,19 @@
 // dark mode utils
-const invertImgs = (function() {
+const invertImgs = (function () {
   const invertImg = img =>
-    (img.style.filter =
-      img.style.filter !== 'invert(100%)' ? 'invert(100%)' : '')
+  (img.style.filter =
+    img.style.filter !== 'invert(100%)' ? 'invert(100%)' : '')
   const invertImgs = imgs => Array.prototype.forEach.call(imgs, invertImg)
   return invertImgs
 })()
 
-function Darkmode() {
+function Darkmode () {
   this.mask = document.getElementById('darkmode-mask')
   this.imgs = document.getElementsByTagName('img')
   this.metaThemeColor = document.querySelector('meta[name=theme-color]')
   this.metaThemeColorCatch = ''
 }
-Darkmode.prototype.turnOnDarkmode = function() {
+Darkmode.prototype.turnOnDarkmode = function () {
   // fix chrome bug
   document.querySelector('html').style.background = '#ffffff'
   this.mask.classList.add('darkmode-mask--dark')
@@ -25,34 +25,34 @@ Darkmode.prototype.turnOnDarkmode = function() {
   this.metaThemeColorCatch = this.metaThemeColor.content
   this.metaThemeColor.setAttribute('content', '#333') // dark-mode tab color #333
 }
-Darkmode.prototype.turnOffDarkmode = function() {
+Darkmode.prototype.turnOffDarkmode = function () {
   this.mask.classList.remove('darkmode-mask--dark')
   invertImgs(this.imgs)
   this.metaThemeColor.content = this.metaThemeColorCatch
 }
 
 var simulateClick = function (elem) {
-	// Create our event (with options)
-	var evt = new MouseEvent('click', {
-		bubbles: true,
-		cancelable: true,
-		view: window
-	});
-	// If cancelled, don't dispatch our event
-	var canceled = !elem.dispatchEvent(evt);
+  // Create our event (with options)
+  var evt = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window
+  });
+  // If cancelled, don't dispatch our event
+  var canceled = !elem.dispatchEvent(evt);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
   // darkmode
-  ;(() => {
+  ; (() => {
     const darkmode = new Darkmode()
-    const { isDarkMode } = localStorage
+    const {isDarkMode} = localStorage
     const checkbox = document.querySelector('input[name=mode]')
     if (isDarkMode && JSON.parse(isDarkMode)) {
       darkmode.turnOnDarkmode()
       checkbox.checked = true
     }
-    checkbox && checkbox.addEventListener('change', function() {
+    checkbox && checkbox.addEventListener('change', function () {
       if (this.checked) {
         darkmode.turnOnDarkmode()
         localStorage.setItem('isDarkMode', true)
@@ -63,27 +63,31 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   })()
 
-  // sidebar
-  ;(() => {
-    let toggle = true;
-    const sidebar = document.querySelector('.sidebar')
-    const sidebarButton = document.querySelector('.sidebar__button')
-    const sidebarMask = document.querySelector('.sidebar__mask')
+    // sidebar
+    ; (() => {
+      let toggle = true;
+      const sidebar = document.querySelector('.sidebar')
+      const sidebarButton = document.querySelector('.sidebar__button')
+      const sidebarMask = document.querySelector('.sidebar__mask')
 
-    sidebarMask.addEventListener('click', function() {
+      sidebarMask.addEventListener('click', function () {
         simulateClick(sidebarButton)
-    })
+      })
 
-    sidebarButton && sidebarButton.addEventListener('click', function() {
-      toggle ? sidebarMask.style.display = 'block' : sidebarMask.style.display = 'none'
-      toggle
-        ? sidebar.classList.add('sidebar--expend')
-        : sidebar.classList.remove('sidebar--expend')
-      toggle
-        ? sidebarButton.classList.add('sidebar__button--expend')
-        : sidebarButton.classList.remove('sidebar__button--expend')
+      sidebarButton && sidebarButton.addEventListener('click', function () {
 
-      toggle = !toggle
-    })
-  })()
+        setTimeout(() => {
+          toggle ? sidebarMask.style.display = 'block' : sidebarMask.style.display = 'none'
+        }, 200)
+
+        toggle
+          ? sidebar.classList.add('sidebar--expend')
+          : sidebar.classList.remove('sidebar--expend')
+        toggle
+          ? sidebarButton.classList.add('sidebar__button--expend')
+          : sidebarButton.classList.remove('sidebar__button--expend')
+
+        toggle = !toggle
+      })
+    })()
 })
